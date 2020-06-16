5a2f174c474d54acffa48212893fc165071bbe79
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-06-16 19:26:47 -0400

    backend

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,19 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-      # identified_by :current_user
+      identified_by :uuid
 
-      # def connect
-      #   self.current_user = find_verified_user
-      # end
-
-      # protected
-      # def find_verified_user
-      #   if current_user = User.find_by(id: cookies.signed[:user_id])
-      #     current_user
-      #   else
-      #     reject_unauthorized_connection
-      #   end
-      # end
+      def connect
+        self.uuid = SecureRandom.urlsafe_base64
+      end
    
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,16 +3,12 @@ class MessagesController < ApplicationController
     before_action :authorize_request
 
     def create
-        # header for Authorization token .... decode... get username
-        # byebug
-        message = Message.new(message_text: params[:message][:message_text], chatroom_id: params[:message][:chatroom_id], user_id: @current_user.id, encryption_id: 1)
+       
+        message = Message.new( message_text: params[:message][:message_text], chatroom_id: params[:message][:chatroom_id], user_id: @current_user.id, encryption_id: 1)
         if message.save
             chat_room = Chatroom.find(message.chatroom_id)
-            # byebug
-            ActionCable.server.broadcast "chatroom_channel_#{chat_room.id}", message
-            
-            render json: chat_room
-        else
+            ActionCable.server.broadcast(  "chatroom_channel", message )
+           else
             render json: {errors: message.errors.full_messages}, status: 400 
         end
 
