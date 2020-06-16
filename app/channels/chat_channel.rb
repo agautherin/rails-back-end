329bb9dcd061a524ef8154ca873f6e51b39f1c95
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-06-16 14:54:23 -0400

    backend

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,43 +1,25 @@
-class ChatChannel < ApplicationCable::Channel
-    def subscribed
-        # if params[:chatroom_id].present?
-        #     stream_from("Chatroom-#{params[:chatroom_id]}")
-        # end
+_#{}patrams{}[]:room_id
+        byebug 
+get_chatroom"
+        chat_room_ r#{}Chatroom.find(id)""
+    
+    
+    
+def receive()data
+        
+        
 
-        chat_room = Chatroom.find(params[:id])
-        stream_for chat_room
+        
 
-    end
+        ActionCable.server.broadcast get_chatroom, format_message(data)
 
-    def chat(message_data)
+    
+    
+devf format_message()
+        
+        
+en    end
 
-        sender = get_sender(message_data[:user_id])
-        chatroom_id = message_data[:chatroom_id]
-        message = message_data[:message_text]
-
-        raise 'There was no room specified' if chatroom_id.blank?
-        chatroom = get_chatroom(chatroom_id) # A conversation is a room
-        raise 'No conversation found!' if chatroom.blank?
-        raise 'No message!' if message.blank?
-
-        chatroom.users << sender unless chatroom.users.include?(sender)
-
-        Message.create(
-            message_text: message,
-            chatroom_id: chatroom,
-            user_id: sender,
-            encryption_id: 1
-          )
-    end
-
-    def get_sender(id)
-        User.find(id)
-    end
-
-    def get_chatroom(id)
-        Chatroom.find(id)
-    end
-
-
-
-end
+    data        {}message: data[]""message,
+        username: current_user.usernamechannel_
+        byebug
